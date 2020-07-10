8b751a4930a900aacd8431341984fb9f671fc9cc
Joe <739727250@qq.com>
2020-07-10 10:34:38 +0800

    添加ssl证书

--- src/main/java/com/qjl/iot/config/ConnectorConfig.java
+++ src/main/java/com/qjl/iot/config/ConnectorConfig.java
@@ -5,6 +5,7 @@ import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -12,30 +13,38 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectorConfig {
+    @Value("${http.port}")
+    private Integer port;
+
+    @Value("${server.port}")
+    private Integer httpsPort;
 
     @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection securityCollection = new SecurityCollection();
-                securityCollection.addPattern("/*");
-                securityConstraint.addCollection(securityCollection);
-                context.addConstraint(securityConstraint);
+                // 如果要强制使用https，请松开以下注释
+                // SecurityConstraint constraint = new SecurityConstraint();
+                // constraint.setUserConstraint("CONFIDENTIAL");
+                // SecurityCollection collection = new SecurityCollection();
+                // collection.addPattern("/*");
+                // constraint.addCollection(collection);
+                // context.addConstraint(constraint);
             }
         };
-        factory.addAdditionalTomcatConnectors(redirectConnector());
-        return factory;
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 添加http
+        return tomcat;
     }
 
-    private Connector redirectConnector() {
-        Connector connector = new Connector(Http11NioProtocol.class.getName());
-        connector.setScheme("http");
-        connector.setPort(80);
+    // 配置http
+    private Connector createStandardConnector() {
+        // 默认协议为org.apache.coyote.http11.Http11NioProtocol
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setSecure(false);
-        connector.setRedirectPort(443);
+        connector.setScheme("http");
+        connector.setPort(port);
+        connector.setRedirectPort(httpsPort); // 当http重定向到https时的https端口号
         return connector;
     }
 }
